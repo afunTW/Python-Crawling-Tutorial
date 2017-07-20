88010bca2b8387199602287903c83e10babf8cf6
afunTW <endlessproof@gmail.com>
2017-07-20 11:44:50 +0800

    add: ptt beauty image crawling practicing

---
+++ Lesson3_non_static_crawling/01_text_to_file/step1_image_crawling.py
@@ -0,0 +1,32 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+from pprint import pprint
+
+
+def download_image(url):
+    image = Image.open(requests.get(url, stream=True).raw)
+    remote_filename = url.split('/')[-1]
+    filename, ext = remote_filename.split('.')
+    saved_filename = '{}.{}'.format(filename, image.format.lower())
+
+    print('download {}'.format(saved_filename))
+    with open(saved_filename, 'wb') as f:
+        _ = requests.get(url, stream=True)
+        for block in _.iter_content(512):
+            if not block: break
+            f.write(block)
+
+def main():
+    url = 'https://www.ptt.cc/bbs/Beauty/M.1500472742.A.E4A.html'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    links = soup.find_all('a', attrs={'href': True})
+
+    for link in links:
+        if re.search('i\.imgur\.com', link['href']):
+            download_image(link['href'])
+
+if __name__ == '__main__':
+    main()
