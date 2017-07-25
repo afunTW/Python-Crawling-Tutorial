c896951722e16db7f4c761e833208a3b8c836dbc
afunTW <endlessproof@gmail.com>
2017-07-26 00:55:43 +0800

    rename directory

---
+++ Session_3/Lab_0_image_crawling/step_bonus_one_requests.py
@@ -0,0 +1,24 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+from pprint import pprint
+
+
+def download_image(url):
+    response = requests.get(url, stream=True)
+    image = Image.open(response.raw)
+    remote_filename = url.split('/')[-1]
+    filename, ext = remote_filename.split('.')
+    saved_filename = '{}.{}'.format(filename, image.format.lower())
+
+    print('download {}'.format(saved_filename))
+    image.save(saved_filename)
+
+def main():
+    url = 'http://imgur.com/rqCqA.png'
+    download_image(url)
+
+
+if __name__ == '__main__':
+    main()
