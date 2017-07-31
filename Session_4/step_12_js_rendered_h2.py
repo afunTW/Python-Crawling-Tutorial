90b23bac793887a2621864c51502396172626a18
afunTW <endlessproof@gmail.com>
2017-07-31 19:41:56 +0800

    get rendered h2 tag by selenium

--- Session_4/step_12_js_rendered_h2.py
+++ Session_4/step_12_js_rendered_h2.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 # 對範例網站送出 request 並把回應的網頁內容送到解析器
 url = 'https://afuntw.github.io/demo-crawling/demo-page/ex4/index1.html'
@@ -8,4 +9,26 @@ soup = BeautifulSoup(response.text, 'lxml')
 
 # 搜尋 id 是 first 的 h2 tag
 h2 = soup.find_all('h2', id="first")
-print(h2[0].text)
+print('static get:', h2[0].text)
+
+
+
+# Selenium
+# 打開 Chrome 瀏覽器
+driver = webdriver.Chrome()
+
+# 將瀏覽器視窗最大化
+driver.maximize_window()
+
+# 等待時間設定為 10 秒, 每個 session 只需要呼叫一次這個 function
+driver.implicitly_wait(10)
+
+# 對目標網站送 request
+driver.get(url)
+
+# 搜尋 id 是 first 的 h2 tag
+selenium_h2 = driver.find_element_by_id('first')
+print('selenium get: ', selenium_h2.text)
+
+# 關掉瀏覽器
+driver.quit()
