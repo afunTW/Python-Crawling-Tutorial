d585d27e7a301f95d64fd74aac58e327a73589c2
afunTW <endlessproof@gmail.com>
2017-08-04 02:44:19 +0800

    modified: urljoin observation sample code

--- Session_C/02_observe_urljoin.py
+++ Session_C/02_observe_urljoin.py
@@ -7,21 +7,22 @@ from urllib.parse import urljoin
 
 url = 'http://140.112.115.12/exam/graduate'
 response = requests.get(url)
-soup = BeautifulSoup(response.text)
+soup = BeautifulSoup(response.text, 'lxml')
 
 # 取得所有 img tag 中 src 符合 application-pdf.png 片段的
 images = soup.find_all('img',{'src': re.compile('application-pdf\.png')})
 
 # 查看網頁網址的拆解片段
-print(response.url, '\n\n', urlparse(response.url), '\n-----\n')
+print(response.url, '\n', urlparse(response.url), '\n-----\n')
 
 # 查看第1張圖片網址的拆解片段
 image_url = images[0].parent['href']
-print(image_url, '\n\n', urlparse(image_url), '\n-----\n')
+print(image_url, '\n', urlparse(image_url), '\n-----\n')
 
 # 取得絕對路徑
 print(urljoin(response.url, image_url))
-#print(urljoin(response.url, '/106g/'))
-#print(urljoin(response.url, '../'))
-#print(urljoin(response.url, '../../'))
-#print(urljoin(response.url, '//facebook.com'))
+# print(urljoin(response.url, '106g/'))
+# print(urljoin(response.url, '/106g/'))
+# print(urljoin(response.url, '//facebook.com'))
+# print(urljoin(response.url, '../'))
+# print(urljoin(response.url, '../../'))
